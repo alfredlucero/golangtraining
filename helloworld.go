a994a26bde5e542e88da7aa89c5c39d7f827b288
alfredlucero <alfred.lucero@ucla.edu>
2021-03-22 16:42:39 -0700

    Added golang helloworld file

---
+++ helloworld.go
@@ -0,0 +1,11 @@
+// Every Go program must be included in a package
+// A standalone executable Go program must have a main function and must be included in the main package
+// main is the entrypoint of execution
+package main
+
+import "fmt"
+
+// go run helloworld.go
+func main() {
+	fmt.Println("Hello world!")
+}
